99f31f2b5a4e8b9b774a977324b68bda9aa5d32f
yunustencere <baytencere@gmail.com>
2022-03-04 12:17:38 +0300

    ciname movie relation created

---
+++ app/Services/Cinema/CinemaService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Services\Cinema;
+
+use App\Models\Cinema;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class CinemaService implements CinemaServiceInterface
+{
+    public function getAll()
+    {
+        return Cinema::all();
+    }
+
+
+    public function store(array $attributes)
+    {
+        $cinema = Cinema::create($attributes);
+        $cinema->movies()->attach($attributes['movie_ids']);
+        return $cinema;
+    }
+
+    public function destroy(int $id)
+    {
+        return Cinema::destroy($id);
+    }
+
+    public function addMovie(array $attributes)
+    {
+        $cinema = Cinema::where('id', $attributes['cinema_id'])->first();
+        $cinema_movies = $cinema->movies()->pluck('id')->toArray();
+        if (in_array($attributes['movie_id'], $cinema_movies))
+            throw new HttpResponseException(response()->json([
+                'errors' => "Film already exists in that cinema."
+            ], 422));
+        $cinema->movies()->attach($attributes['movie_id']);
+        return Cinema::where('id', $attributes['cinema_id'])->with('movies')->first();
+    }
+
+    public function removeMovie(array $attributes)
+    {
+        $cinema = Cinema::where('id', $attributes['cinema_id'])->first();
+        if ($cinema->movies()->count() <= 2)
+            throw new HttpResponseException(response()->json([
+                'errors' => "A cinema must have at least 2 movies."
+            ], 422));
+        $cinema->movies()->detach($attributes['movie_id']);
+        return Cinema::where('id', $attributes['cinema_id'])->with('movies')->first();
+    }
+
+    public function withMovies()
+    {
+        return Cinema::with('movies')->get();
+    }
+}
